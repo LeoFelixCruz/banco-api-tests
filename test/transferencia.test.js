67b6c84b45f331f5bf71ff2cb826ca312f07b2c2
LeoFelixCruz <felixcruz@live.com>
2025-09-24 13:58:09 -0300

    Adicionei testes para GET transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,12 +5,13 @@ const {obterToken} = require('../helpers/autenticacao')
 const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
-    describe('POST /transferencias', () => {
         let token
 
         beforeEach( async () => {
             token = await obterToken('julio.lima', '123456') 
         })
+    describe('POST /transferencias', () => {
+
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async ()=>{ 
            
@@ -39,4 +40,31 @@ describe('Transferências', () => {
             expect(resposta.status).to.equal(422);
         })
         })
+    
+    describe('GET /transferencias/{id}', ()=>{
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando o id for válido', async ()=>{
+            const resposta = await request(process.env.BASE_URL)    
+            .get('/transferencias/11')
+            .set('Authorization', `Bearer ${token}`)
+            
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(11)
+            expect(resposta.body.conta_origem_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(2)
+            expect(resposta.body.valor).to.equal(11.00)  // Existe um erro na API - consta que é para vir number no schema.
+        })
+
+    describe('GET /transferencias', ()=>{
+        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async ()=>{
+            const resposta = await request(process.env.BASE_URL)
+            .get('/transferencias?page=1&limit=10')
+            .set('Authorization', `Bearer ${token}`)
+            
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.lengthOf(10)
+
+        })
+    })
+    })
     })
